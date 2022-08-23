3fdd4e019d44fd2218dac04f9222a1eef34bfdac
Emiliano Scanga <scangaemiliano@gmail.com>
2022-08-23 15:51:36 +0200

    Aggiunto la barra di ricerche su Markets

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -11,6 +11,7 @@ const columns = [
     {
         name: 'Totals',
         selector: row => row.total,
+        sortable: true,
     },
 ]
 
--- src/components/Markets.js
+++ src/components/Markets.js
@@ -33,10 +33,14 @@ export function Markets(){;
     const [ searchParams ] = useSearchParams();
     const filterPage = searchParams.get('base_assets');
     const [data, setData] = useState([]);
+    const [originalData, setOriginalData] = useState([]);
 
     const { data: data1, loading: loading1, error: error1, reaload: reload1} = FetchApi('https://api.binance.com/api/v3/ticker/price');
     const { data: data2, loading: loading2, error: error2, reaload: reload2} = FetchApi('https://api.binance.com/api/v3/exchangeInfo');
     const { data: data3, loading: loading3, error: error3, reaload: reload3} = FetchApi('https://api.binance.com/api/v3/ticker/24hr');
+
+    const [input, setInput] = useState("");
+
 
     useEffect(()=>{
         if(data1 && data2 && data3){
@@ -56,13 +60,38 @@ export function Markets(){;
                 _data = _data.filter(data => data.baseAsset == filterPage)
             }
             setData(_data);
+            setOriginalData(_data);
         }
     }, [data1, data2, data3]);
+
+    const handleInput = (event) =>{
+        setInput(event.target.value);
+    }
+
+    const handleSearch = (input) =>{
+        const _list = [...data];
+
+        if(input !== ""){
+          const filtered = _list.filter((value)=>value.symbol.match(new RegExp(input,"ig")))
+          setData(filtered);
+        }else{
+            setData(originalData);
+        }}
+
+    useEffect(()=>{
+        handleSearch(input)
+    },[input, data]);
+
+
 
     if(loading1 && loading2 && loading3) return <p>Loading...</p>
 
     return(
-        <>
+        <> 
+            <div>
+            <label for="searchInput">Search your coin: </label>
+            <input name="searchInput" onInput={handleInput} value={input} />
+            </div>
             <KitchenSinkStory
                     columns={columns}
                     data={data}
